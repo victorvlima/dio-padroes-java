af18edc742fac31fa33025aad7ef35101fdbefad
Victor Lima <1613658+victorvlima@users.noreply.github.com>
2025-09-09 09:43:09 -0300

    feat: projeto base com Spring Initializr via VS Code

---
+++ src/main/java/br/com/dio/projeto/padroes/dio_padroes_java/DioPadroesJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.projeto.padroes.dio_padroes_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPadroesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPadroesJavaApplication.class, args);
+	}
+
+}
